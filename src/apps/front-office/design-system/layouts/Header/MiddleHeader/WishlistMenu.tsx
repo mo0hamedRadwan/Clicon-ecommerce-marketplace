12f9f27e55dd085fd96aceb2f1d1ee1c6fb5c0b0
mo0hamedRadwan <mo0hamedradwan@yahoo.com>
2024-08-08 20:03:08 +0300

    add wishlist

--- src/apps/front-office/design-system/layouts/Header/MiddleHeader/WishlistMenu.tsx
+++ src/apps/front-office/design-system/layouts/Header/MiddleHeader/WishlistMenu.tsx
@@ -1,3 +1,45 @@
+import { trans } from "@mongez/localization";
+import { Link } from "@mongez/react-router";
+import Button from "apps/front-office/design-system/components/Button";
+import { isRTL } from "apps/front-office/utils/helpers";
+import URLS from "apps/front-office/utils/urls";
+import { cartProducts } from "shared/data/testData";
+
 export default function WishlistMenu() {
-  return <div></div>;
+  return (
+    <div
+      className={`hidden group-hover:block z-20 absolute top-[50px] ${isRTL() ? "left-0" : "right-0"} w-[300px] p-4 bg-white text-black text-base rounded shadow-2`}>
+      <h3 className="text-xl font-semibold center-y gap-x-1">
+        <span>{trans("wishlist")}</span>
+        <span>(02)</span>
+      </h3>
+      <div className="mt-3 w-full h-[1px] bg-gray-300"></div>
+      <ul className="py-3 flex flex-col gap-y-3">
+        {cartProducts.map(product => (
+          <li key={product.id} className="center-y">
+            <Link
+              to={"/productId"}
+              className="flex-grow h-[60px] center-y gap-x-5">
+              <div className="w-[60px] h-full">
+                <img
+                  src={product.imageUrl}
+                  alt={product.name}
+                  className="w-full h-full object-cover rounded-md border border-gray-200"
+                />
+              </div>
+              <p className="h-full flex-grow">{product.name}</p>
+            </Link>
+            {/* onClick Delete Product from cart */}
+            <button className="text-gray">x</button>
+          </li>
+        ))}
+      </ul>
+      <div className="mb-3 w-full h-[1px] bg-gray-300"></div>
+      <div className="mt-2 flex flex-col gap-y-2">
+        <Button variant="contained" href={URLS.checkout}>
+          {trans("wishlist").toUpperCase()}
+        </Button>
+      </div>
+    </div>
+  );
 }
